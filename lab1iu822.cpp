ac8dd0bcfe44c34261ce1c5f9b3ce4fc8c73ce20
Lerasinevich <Akako-Amber@yandex.ru>
2014-02-25 16:37:26 +0400

    Lab1

---
+++ lab1iu822.cpp
@@ -0,0 +1,20 @@
+// lab1iu822.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "iostream"
+
+using namespace std;
+
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int x;
+	int y;
+	int result  = x+y;
+	cout:
+	return 0;
+}
+
